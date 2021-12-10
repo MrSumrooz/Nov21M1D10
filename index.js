1ea081d5bcf880736614d0549cc621b5ad85afd8
MrSumrooz <93378338+MrSumrooz@users.noreply.github.com>
2021-12-10 15:22:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,34 @@ console.log(student)
 delete student.email
 
 console.log(student)
+
+let carsArray = 
+            ["ford","Honda",
+            "TeslaLuxury", "BMWLuxury ",
+             "FerrariLuxury", "FordMass",
+            " PorscheLuxury ", "HondaMass-",
+                "Lamborghini", "ToyotaMass", 
+            ]
+
+console.log(carsArray)
+
+
+let randomArrayNumber = [];
+
+for (i = 0; i <= 100 ; i++ ){
+
+    
+
+    
+    return randomArrayNumber.push(Math.round(Math.random() * 100))
+    
+    
+
+    
+    
+}
+
+console.log(randomArrayNumber)
+
+
+
