80d2ab26e025a607f3dcb1c33913ffccf5e83390
MrSumrooz <93378338+MrSumrooz@users.noreply.github.com>
2021-12-10 13:49:29 +0100

    js

---
+++ index.js
@@ -0,0 +1,19 @@
+let x = "John"
+let y = "Doe"
+
+console.log("< "+ x + " " + y +" >")
+
+let student = {
+
+    name : "Sumrooz",
+    surname : "Zia",
+    email : "sumrooz.zia@gail.com",
+
+
+}
+
+console.log(student)
+
+delete student.email
+
+console.log(student)
